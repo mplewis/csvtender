c4c75ee365b805b02e32c0fcb69876dd29d2443f
Matt Lewis <matt@mplewis.com>
2018-03-24 14:39:38 -0600

    docs: yardoc, gemspec

--- csvtender.gemspec
+++ csvtender.gemspec
@@ -1,6 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'csvtender'
   s.version = '0.0.0'
+  s.summary = 'Manipulate CSV files with a simple DSL'
+
+  s.homepage = 'https://github.com/mplewis/csvtender'
+  s.authors = ['Matt Lewis']
+  s.email = 'matt@mplewis.com'
+
   s.files = ["lib/csvtender.rb"]
   s.license = 'MIT'
 end
--- lib/csvtender.rb
+++ lib/csvtender.rb
@@ -1,4 +1,6 @@
 class CSVtender
+  # Creates a new CSVtender.
+  # @param table [Array] Optional: initial data for the table
   def initialize(table = [])
     @table = table
     @orientation = :standard
@@ -18,38 +20,46 @@ class CSVtender
     @table
   end
 
+  # Orients the table traditionally - table contains rows which contain cells.
   def rows
     @orientation = :standard
     self
   end
 
+  # Orients the table transposed - table contains columns which contain cells.
   def cols
     @orientation = :vertical
     self
   end
 
+  # Outputs the table as an array of rows.
   def show
     table = nil
     oriented { table = @table }
     table
   end
 
+  # The number of rows this table contains.
   def count
     show.count
   end
 
+  # Maps each row or column in this table over a user-provided function.
   def map(mutator = nil, &block)
     delegate :map!, mutator, &block
   end
 
+  # Calls the user-provided function and includes only items for which the function returns truthy.
   def select(mutator = nil, &block)
     delegate :select!, mutator, &block
   end
 
+  # Calls the user-provided function and rejects items for which the function returns truthy.
   def reject(mutator = nil, &block)
     delegate :reject!, mutator, &block
   end
 
+  # Treat this table as if the first row is a header row.
   def with_headers
     return self if @headers
     oriented do
@@ -60,6 +70,7 @@ class CSVtender
     self
   end
 
+  # Treat this table as if it has no header row.
   def without_headers
     return self unless @headers
     @headers = false
@@ -75,17 +86,20 @@ class CSVtender
     @orientation == :vertical
   end
 
+  # Helper method. Pass it a block and it will always call the block with the table in standard orientation.
   def oriented
     transpose if vertical?
     yield
     transpose if vertical?
   end
 
+  # Transposes the table data between row and column orientation. Calling this twice in a row is a no-op.
   def transpose
     head, *tail = @table
     @table = head.zip(*tail)
   end
 
+  # Helper method. Handles both styles of user-provided block.
   def delegate(method, mutator = nil, &block)
     raise ArgumentError, 'must provide mutator or block' unless block_given? || mutator
     block ||= -> (item) { mutator.call item }
